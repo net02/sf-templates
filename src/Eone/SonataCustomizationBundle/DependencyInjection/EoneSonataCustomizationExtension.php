9bedec993a92bd1f669f4f183a9fecfa2065edec
Daniele Bartocci <daniele.bartocci@e-one.it>
2014-07-10 17:49:46 +0200

    locales array

--- src/Eone/SonataCustomizationBundle/DependencyInjection/EoneSonataCustomizationExtension.php
+++ src/Eone/SonataCustomizationBundle/DependencyInjection/EoneSonataCustomizationExtension.php
@@ -30,5 +30,6 @@ class EoneSonataCustomizationExtension extends Extension
         }
         // prepare locale requirements for routing purposes
         $container->setParameter('eone.locale.requirements', implode('|', array_keys($container->getParameter('locales_available'))));
+        $container->setParameter('eone.locale.locales', array_keys($container->getParameter('locales_available')));
     }
 }
